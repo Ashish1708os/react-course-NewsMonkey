529da6d12b2b4b170a4d50ec554e81c4eff4582f
Ashish Salve <ashishsalve1777@gmail.com>
2021-10-04 20:08:06 +0530

    video 22 newsMonkey Project setup

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+import React, { Component } from "react";
+
+export default class App extends Component {
+  c = "ashish";
+  render() {
+    return <div>hello my first class based component {this.c}</div>;
+  }
+}
