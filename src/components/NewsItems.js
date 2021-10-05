3dcf1ccc9c185d2bc9a7c54a5b92191d23edca72
Ashish Salve <ashishsalve1777@gmail.com>
2021-10-05 14:56:59 +0530

    video 24 newsapi sample output and setting component structure

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -2,7 +2,25 @@ import React, { Component } from "react";
 
 export class NewsItems extends Component {
   render() {
-    return <div>this is a newsItem</div>;
+    let { title, description } = this.props;
+    return (
+      <div className="my-1 mx-1">
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src="https://i.gadgets360cdn.com/large/windows_11_image_microsoft_1629357765824.jpg"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
