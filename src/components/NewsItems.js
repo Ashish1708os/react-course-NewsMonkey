b8aed498d269e454d0bfc7084448ebf5c7a547f7
Ashish Salve <ashishsalve1777@gmail.com>
2021-10-05 20:13:44 +0530

    video 25 using state in class based component

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -2,20 +2,16 @@ import React, { Component } from "react";
 
 export class NewsItems extends Component {
   render() {
-    let { title, description } = this.props;
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
-      <div className="my-1 mx-1">
+      <div className="my-3 mx-1">
         <div className="card" style={{ width: "18rem" }}>
-          <img
-            src="https://i.gadgets360cdn.com/large/windows_11_image_microsoft_1629357765824.jpg"
-            className="card-img-top"
-            alt="..."
-          />
+          <img src={imageUrl} className="card-img-top" alt="newsImage" />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/" className="btn btn-primary">
-              Go somewhere
+            <a href="/newsDetails/" className="btn btn-sm btn-primary">
+              Read More
             </a>
           </div>
         </div>
