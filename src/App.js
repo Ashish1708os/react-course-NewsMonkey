8d2d510c82a0453a906e83371527ed088f194843
Ashish Salve <ashishsalve1777@gmail.com>
2021-10-08 23:29:26 +0530

    video 31 fetching news chategory wise

--- src/App.js
+++ src/App.js
@@ -2,13 +2,42 @@ import "./App.css";
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import News from "./components/News";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
-        <News pageSize={6} country="in" category="general" />
+        <Router>
+          <Navbar />
+          {/* <News pageSize={6} country="in" category="general" /> */}
+          <Switch>
+            <Route exact key="home" path="/">
+              <News pageSize={6} country="in" category="general" />
+            </Route>
+            <Route exact key="business" path="/business">
+              <News pageSize={6} country="in" category="business" />
+            </Route>
+            <Route exact key="entertainment" path="/entertainment">
+              <News pageSize={6} country="in" category="entertainment" />
+            </Route>
+            <Route exact key="general" path="/general">
+              <News pageSize={6} country="in" category="general" />
+            </Route>
+            <Route exact key="health" path="/health">
+              <News pageSize={6} country="in" category="health" />
+            </Route>
+            <Route exact key="science" path="/science">
+              <News pageSize={6} country="in" category="science" />
+            </Route>
+            <Route exact key="sports" path="/sports">
+              <News pageSize={6} country="in" category="sports" />
+            </Route>
+            <Route exact key="technology" path="/technology">
+              <News pageSize={6} country="in" category="technology" />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     );
   }
